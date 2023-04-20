21094de69d8ba85b4c2fa550d01b4627330623d2
sermol2001 <ser-mol2001@yandex.ru>
2023-04-21 00:24:26 +0300

    add tcp keep-alive

--- Client-Server-Application/processing_server.cpp
+++ Client-Server-Application/processing_server.cpp
@@ -7,7 +7,6 @@
 #include <stdexcept>
 #include <WinSock2.h>
 #include <WS2tcpip.h>
-
 #pragma comment(lib, "ws2_32.lib")
 
 class DataProcessingServer {
@@ -18,22 +17,22 @@ public:
 
         // Initialize Winsock
         if (WSAStartup(MAKEWORD(2, 2), &m_wsaData) != 0) {
-            throw std::runtime_error("Failed to initialize Winsock.");
+            std::cerr << "Failed to initialize Winsock.\n";
         }
 
         // Create listening socket
         m_listenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
         if (m_listenSocket == INVALID_SOCKET) {
-            throw std::runtime_error("Failed to create socket.");
+            std::cerr << "Failed to create socket.\n";
         }
 
         // Bind socket to address
         sockaddr_in addr;
         addr.sin_family = AF_INET;
-        addr.sin_addr.s_addr = inet_addr(m_host.c_str());
+        addr.sin_addr.s_addr = INADDR_ANY; // inet_addr(m_host.c_str());
         addr.sin_port = htons(m_port);
-        if (bind(m_listenSocket, reinterpret_cast<sockaddr *>(&addr), sizeof(addr)) == SOCKET_ERROR) {
-            throw std::runtime_error("Failed to bind socket to address.");
+        if (bind(m_listenSocket, reinterpret_cast<SOCKADDR *>(&addr), sizeof(addr)) == SOCKET_ERROR) {
+            std::cerr << "Failed to bind socket to address.\n";
         }
     }
 
@@ -47,25 +46,30 @@ public:
 
         // Listen for incoming connections
         if (listen(m_listenSocket, SOMAXCONN) == SOCKET_ERROR) {
-            throw std::runtime_error("Failed to listen for incoming connections.");
+            std::cerr << "Failed to listen for incoming connections: " << WSAGetLastError() << "\n";
+            throw std::runtime_error("");
         }
 
         // Create an event object for the listening socket
-        HANDLE listenEvent = WSACreateEvent();
+        WSAEVENT listenEvent = WSACreateEvent();
         if (listenEvent == WSA_INVALID_EVENT) {
-            throw std::runtime_error("Failed to create event object.");
+            std::cerr << "Failed to create event object: " << WSAGetLastError() << '\n';
+            throw std::runtime_error("");
         }
 
         // Associate the event object with the listening socket
         if (WSAEventSelect(m_listenSocket, listenEvent, FD_ACCEPT) == SOCKET_ERROR) {
-            throw std::runtime_error("Failed to associate event object with the listening socket");
+            std::cerr << "Failed to associate event object with the listening socket: " << WSAGetLastError() << '\n';
+            throw std::runtime_error("");
         }
+
 
         // Create a buffer for receiving data from clients
         char buffer[m_buffer_size];
 
         // Create a vector to store clients sockets
         std::vector<SOCKET> clientSockets;
+        clientSockets.push_back(m_listenSocket);
 
         // Create a vector to hold event objects
         std::vector<WSAEVENT> events;
@@ -73,31 +77,39 @@ public:
         while (true) {
 
             // resize the events vector to hold the current number of sockets
-            events.resize(clientSockets.size() + 1);
+            events.resize(clientSockets.size());
             events[0] = listenEvent;
 
+            std::cout << "Number of sockets: " << clientSockets.size() << "\n";
+
             // Add client sockets to the array of event objects
-            for (size_t i = 0; i < clientSockets.size(); ++i) {
-                events[i + 1] = WSACreateEvent();
-                if (events[i + 1] == WSA_INVALID_EVENT) {
+            for (size_t i = 1; i < clientSockets.size(); ++i) {
+                events[i] = WSACreateEvent();
+                if (events[i] == WSA_INVALID_EVENT) {
                     throw std::runtime_error("Failed to create event object.");
                 }
-                if (WSAEventSelect(clientSockets[i], events[i + 1], FD_READ | FD_CLOSE) == SOCKET_ERROR) {
+                if (WSAEventSelect(clientSockets[i], events[i], FD_READ | FD_CLOSE) == SOCKET_ERROR) {
                     throw std::runtime_error("Failed to associate event object with client socket.");
                 }
             }
 
+
             // Wait for events to occur
-            DWORD eventCount = WSAWaitForMultipleEvents(clientSockets.size() + 1, events.data(), FALSE, WSA_INFINITE, FALSE);
-            if (eventCount == WSA_WAIT_FAILED) {
-                throw std::runtime_error("Wait for events failed.");
-            }
+            std::cout << "Start waiting for events\n";
+            DWORD eventCount = WSAWaitForMultipleEvents(clientSockets.size(), events.data(),
+                    FALSE, WSA_INFINITE, FALSE);
+            std::cout << "End waiting for events\n";
+            int eventIndex = eventCount - WSA_WAIT_EVENT_0;
+
+            WSANETWORKEVENTS networkEvents;
+            WSAEnumNetworkEvents(clientSockets[eventIndex], events[eventIndex], &networkEvents);
 
             // Process events
             if (eventCount == WSA_WAIT_FAILED) {
                 throw std::runtime_error("WSAWaitForMultipleEvents failed.");
 
-            } else if (eventCount == WSA_WAIT_EVENT_0) {
+            } else if (eventIndex == 0 && networkEvents.lNetworkEvents & FD_ACCEPT) {
+
                 // Accept a new connection
                 sockaddr_in clientAddress;
                 int clientAddressLength = sizeof(clientAddress);
@@ -107,24 +119,62 @@ public:
                     throw std::runtime_error("Failed to accept connection.");
                 }
                 clientSockets.push_back(clientSocket);
-                std::cout << "Client is connected.\n";
-            } else {
-                int clientSocket = clientSockets[WSA_WAIT_EVENT_0 - 1];
+                std::cout << "Client is connected: " << "*:" << clientAddress.sin_port << '\n';
+
+                int keepAlive = 1;
+                int keepAliveInterval = 5;
+                int keepAliveTime = 10;
+
+                // Set keep alive option
+                if (setsockopt(clientSocket, SOL_SOCKET, SO_KEEPALIVE,
+                        reinterpret_cast<char*>(&keepAlive),
+                        sizeof(keepAlive)) != 0) {
+                    std::cerr << "Failed to set keep-alive option: " << WSAGetLastError() << "\n";
+                    throw std::runtime_error("");
+                }
+
+                if (setsockopt(clientSocket, IPPROTO_TCP, TCP_KEEPIDLE,
+                        reinterpret_cast<char*>(&keepAliveTime),
+                        sizeof(keepAliveTime)) != 0) {
+                    std::cerr << "Failed to set keep-alive time: " << WSAGetLastError() << "\n";
+                    throw std::runtime_error("");
+                }
+
+                if (setsockopt(clientSocket, IPPROTO_TCP, TCP_KEEPINTVL,
+                        reinterpret_cast<char*>(&keepAliveInterval),
+                        sizeof(keepAliveInterval)) != 0) {
+                    std::cerr << "Failed to set keep-alive interval time: " << WSAGetLastError() << "\n";
+                    throw std::runtime_error("");
+                }
+
+            } else if (networkEvents.lNetworkEvents & FD_READ) {
+                int clientSocket = clientSockets[eventIndex];
                 int bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
                 if (bytesReceived == SOCKET_ERROR) {
-                    throw std::runtime_error("Failed to receive data from client.");
+                    std::cerr << "Failed to receive data from client: " << WSAGetLastError() << '\n';
+                    throw std::runtime_error("");
                 } else if (bytesReceived == 0) {
                     std::cout << "Client disconnected.\n";
                     closesocket(clientSocket);
-                    WSACloseEvent(events[WSA_WAIT_EVENT_0]);
-                    clientSockets.erase(clientSockets.begin() + WSA_WAIT_EVENT_0 - 1);
+                    WSACloseEvent(events[eventIndex]);
+                    clientSockets.erase(clientSockets.begin() + eventIndex);
                 } else {
                     // Send data to the display server
                     std::cout << "Received data from client: " << buffer << "\n";
                 }
+
+            } else if (networkEvents.lNetworkEvents & FD_CLOSE) {
+                // Other socket end is closed
+                int clientSocket = clientSockets[eventIndex];
+                closesocket(clientSocket);
+                WSACloseEvent(events[eventIndex]);
+                clientSockets.erase(clientSockets.begin() + eventIndex);
+
+                std::cout << "Client disconnected\n";
             }
         }
     }
+
 
 private:
     std::string m_host;
