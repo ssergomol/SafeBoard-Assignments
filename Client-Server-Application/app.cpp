59aae60fce5ccc90a7e9ab4da4452987b50fe24b
sermol2001 <ser-mol2001@yandex.ru>
2023-04-21 03:47:38 +0300

    implement all functionality for client

--- Client-Server-Application/app.cpp
+++ Client-Server-Application/app.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
+#include <WinSock2.h>
+#include <cerrno>
+
+#pragma comment(lib, "ws2_32.lib")
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "WSAStartup failed with error: " << WSAGetLastError() << std::endl;
+        return 1;
+    }
+
+    SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (clientSocket == INVALID_SOCKET) {
+        std::cerr << "socket creation failed with error: " << WSAGetLastError() << std::endl;
+        WSACleanup();
+        return 1;
+    }
+
+    sockaddr_in serverAddress{};
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(8080); // port number of the server
+    serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1"); // IP address of the server
+
+    if (connect(clientSocket, reinterpret_cast<sockaddr*>(&serverAddress), sizeof(serverAddress)) == SOCKET_ERROR) {
+        std::cerr << "connection failed with error: " << WSAGetLastError() << std::endl;
+        closesocket(clientSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    std::string message = "Hey!";
+    if (send(clientSocket, message.c_str(), message.length(), 0) == SOCKET_ERROR) {
+        std::cerr << "send failed with error: " << WSAGetLastError() << std::endl;
+        closesocket(clientSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    closesocket(clientSocket);
+    WSACleanup();
     return 0;
 }
